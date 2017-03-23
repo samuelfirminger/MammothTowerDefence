8afe8cc85fe4814f3a41d79ed7ea1c4a15b7a64e
Peter <ptmfitch@gmail.com>
2017-03-23 13:49:18 +0000

    V0.9.1 Spawn Engine Improved
    -enemies now spawn in groups
    -waves are a series of groups
    -the game lasts a certain number of waves

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -10,9 +10,18 @@ public class EnemySpawnManager : MonoBehaviour {
     public Transform enemyFastPrefab;
     public Transform enemySlowPrefab;
   
-    public float timeBetweenWaves = 5.5f;
-    private float countdown = 2f;
-    private int waveIndex = 3;
+    private float timeBetweenWaves = 45f;
+	private float waveCooldown;
+	private float timeBetweenGroups = 6f;
+	private float timeBetweenSpawns = 0.2f;
+	//How many waves in a game
+	private int gameLength = 3;
+	//Which wave you are on
+	private int waveIndex;
+	//How many groups in a wave
+	private int waveSize = 5;
+	//How many spawns in a group
+	private int groupSize = 5;
     
     public Transform spawnPoint;
     
@@ -20,34 +29,39 @@ public class EnemySpawnManager : MonoBehaviour {
         //Load all enemy types into array
         enemyTypes[0] = enemyBasicPrefab;
         //enemyTypes[1] = enemyFastPrefab;
-        //enemyTypes[2] = enemySlowPrefab;        
+        //enemyTypes[2] = enemySlowPrefab;
+
+		//Initial time at newgame before enemies begin to spawn
+		//TODO replace this with an unlimited "build phase"
+		waveCooldown = 10f;
+		waveIndex = 0;
     }
     
     void Update() {
-        //Start spawning when countdown done
-        if(countdown <= 0f) {
-            //Calling a Co-routine (using IEnumerator to wait)
-            StartCoroutine(SpawnWave());
-            countdown = timeBetweenWaves;
+		if(waveCooldown <= 0f && waveIndex < gameLength) {
+			StartCoroutine(spawnWave());
+			waveCooldown = timeBetweenWaves;
+			waveIndex++;
+		}
+		waveCooldown -= Time.deltaTime;
+    }
+
+	IEnumerator spawnWave() {
+		for (int i = 0; i < waveSize; i++) {
+			StartCoroutine(spawnGroup());
+			yield return new WaitForSeconds(timeBetweenGroups);
+		}
+	}
+		
+    IEnumerator spawnGroup() {
+        for(int i = 0 ; i < groupSize ; i++) {
+            spawnEnemy();
+            yield return new WaitForSeconds(timeBetweenSpawns);
         }
-        
-        //deltaTime: time since last frame
-        countdown -= Time.deltaTime;
     }
     
-    //IEnumerator: allows this code to be paused
-    //Prevents enemies being created on top of one another
-    IEnumerator SpawnWave() {
-        //waveIndex++;
-        waveIndex = 3;
-        for(int i=0 ; i<waveIndex ; i++) {
-            SpawnEnemy();
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-    
-    void SpawnEnemy() {
-        //Get a random num corresponding to an enemy type, and spawn
+    void spawnEnemy() {
+        //Spawn random enemy from list of enemy types
         var index = Random.Range(0, enemyTypes.Length);
         Instantiate(enemyTypes[index], spawnPoint.position, spawnPoint.rotation);
     }
