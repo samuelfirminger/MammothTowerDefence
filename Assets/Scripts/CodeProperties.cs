75f858271505d5a10d61dad070d4a0d46454a0fa
Peter <ptmfitch@gmail.com>
2017-05-08 13:42:12 +0100

    V1.14.1 Code Properties
    1. Added initial C# classes for tracking "code" attributes

---
+++ Assets/Scripts/CodeProperties.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Ways of identifying "good" and "bad" code
+public struct CodeProperties {
+
+	//the extension of the file
+	//eg .zip, .exe etc.
+	string extension;
+
+	int speed;
+	int size;
+
+	bool encryption;
+
+	//the author of the file
+	CodeSource source;
+
+}
