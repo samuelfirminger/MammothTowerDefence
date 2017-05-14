fde34161623bc25621dffe1feff987d4ec58f1e4
samuelfirminger <sf13110@my.bristol.ac.uk>
2017-05-14 19:55:33 +0100

    Music Added
     - Music added. To add a music toggle button, add a button, tag it as "MusicToggle". Create an empty game object in scene, attach Pause Music Script. For the onclick event, add Pause Music Script and choose toggleMusic() function.

---
+++ Assets/Scripts/PauseMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMusic : MonoBehaviour {
+
+
+	AudioSource audiotoPause ; 
+
+	void Awake() { 
+		audiotoPause = GameObject.FindGameObjectWithTag ("GameMusic").GetComponent<AudioSource> (); 
+	}
+
+	public void toggleMusic() {
+		PlaylistManager.instance.pauseMusic (); 
+	}
+
+
+}
